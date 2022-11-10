4f18af6efd52cd82eca8262c5dc39a96ef2b9314
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-10 11:39:50 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -6,7 +6,6 @@
 /**
  * get_val - gets the relative value of a card from it's string value
  * @str: string value of the card
- *
  * Return: relative value of the card (0 through 12)
  */
 int get_val(const char *str)
